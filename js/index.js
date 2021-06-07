9d488e550675aada1f48292d84317ffd528dd3d7
Raynaldo Sutisna <raaynaldo@hotmail.com>
2021-06-06 23:41:17 -0400

    feat: lesson 4-3 with stretch goals 1

--- js/index.js
+++ js/index.js
@@ -1,30 +1,30 @@
 // Insert Copyright Text in Footer
 const today = new Date();
 const thisYear = today.getFullYear();
-console.log(today);
-console.log(thisYear);
+// console.log(today);
+// console.log(thisYear);
 
-const footer = document.querySelector('footer');
+const footer = document.querySelector("footer");
 
-const copyright = document.createElement('p');
+const copyright = document.createElement("p");
 copyright.innerHTML = `&#169; <span>Raynaldo</span> ${thisYear}`;
 
 footer.appendChild(copyright);
 
 // Create List of Skills
 const skills = [
-  'Javascript',
-  'React',
-  'Ruby',
-  'Rails',
-  'C#',
-  'ASP.NET',
-  'HTML',
-  'CSS',
+  "Javascript",
+  "React",
+  "Ruby",
+  "Rails",
+  "C#",
+  "ASP.NET",
+  "HTML",
+  "CSS",
 ];
 
-const skillsSection = document.getElementById('skills');
-const skillsList = skillsSection.getElementsByTagName('ul')[0];
+const skillsSection = document.getElementById("skills");
+const skillsList = skillsSection.getElementsByTagName("ul")[0];
 
 //Using forEach
 // skills.forEach((s) => {
@@ -35,7 +35,49 @@ const skillsList = skillsSection.getElementsByTagName('ul')[0];
 
 //Using for
 for (let i = 0; i < skills.length; i++) {
-  const skill = document.createElement('li');
+  const skill = document.createElement("li");
   skill.innerText = skills[i];
   skillsList.appendChild(skill);
 }
+
+// Lesson 4-3
+
+const messageForm = document.getElementsByName("leave_message")[0];
+messageForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const name = event.target.name.value;
+  const email = event.target.email.value;
+  const message = event.target.message.value;
+
+  const messageSection = document.getElementById("messages");
+  const messageList = messageSection.getElementsByTagName("ul")[0];
+
+  const newMessage = document.createElement("li");
+  newMessage.innerHTML = `<a href="mailto:${email}">${name}</a><span> wrote: ${message} </span>`;
+
+  const removeButton = document.createElement("button");
+  removeButton.innerText = "remove";
+  removeButton.type = "button";
+  removeButton.addEventListener("click", (event) => {
+    const entry = event.target.parentNode;
+    entry.remove();
+    hideOrUnhide();
+  });
+
+  messageList.appendChild(newMessage);
+  newMessage.appendChild(removeButton);
+  hideOrUnhide();
+  messageForm.reset();
+});
+
+function hideOrUnhide() {
+  const messageSection = document.getElementById("messages");
+  const messageList = messageSection.getElementsByTagName("ul")[0];
+  if (messageList.children.length === 0) {
+    messageSection.style.display = "none";
+  } else {
+    messageSection.style.display = "block";
+  }
+}
+
+hideOrUnhide();
