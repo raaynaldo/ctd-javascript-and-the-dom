a1ee1e3a98ec7158470f3da4cec8a36c90cfc85f
Raynaldo Sutisna <raaynaldo@gmail.com>
2021-06-20 12:10:29 -0400

    feat: plain HTML and setup css

--- js/index.js
+++ js/index.js
@@ -4,27 +4,27 @@ const thisYear = today.getFullYear();
 // console.log(today);
 // console.log(thisYear);
 
-const footer = document.querySelector("footer");
+const footer = document.querySelector('footer div');
 
-const copyright = document.createElement("p");
+const copyright = document.createElement('p');
 copyright.innerHTML = `&#169; <span>Raynaldo</span> ${thisYear}`;
 
 footer.appendChild(copyright);
 
 // Create List of Skills
 const skills = [
-  "Javascript",
-  "React",
-  "Ruby",
-  "Rails",
-  "C#",
-  "ASP.NET",
-  "HTML",
-  "CSS",
+    'Javascript',
+    'React',
+    'Ruby',
+    'Rails',
+    'C#',
+    'ASP.NET',
+    'HTML',
+    'CSS'
 ];
 
-const skillsSection = document.getElementById("skills");
-const skillsList = skillsSection.getElementsByTagName("ul")[0];
+const skillsSection = document.getElementById('skills');
+const skillsList = skillsSection.getElementsByTagName('ul')[0];
 
 //Using forEach
 // skills.forEach((s) => {
@@ -35,95 +35,96 @@ const skillsList = skillsSection.getElementsByTagName("ul")[0];
 
 //Using for
 for (let i = 0; i < skills.length; i++) {
-  const skill = document.createElement("li");
-  skill.innerText = skills[i];
-  skillsList.appendChild(skill);
+    const skill = document.createElement('li');
+    skill.innerText = skills[i];
+    skillsList.appendChild(skill);
 }
 
 // Lesson 4-3
 
-const editButton = document.createElement("button");
-editButton.textContent = "edit";
-editButton.type = "button";
-editButton.addEventListener("click", (e) => {
-  const li = e.target.parentNode;
-  const email = li.children[0].href.slice(7);
-  const name = li.children[0].textContent;
-  const comment = li.children[1].textContent.slice(8);
+const editButton = document.createElement('button');
+editButton.textContent = 'edit';
+editButton.type = 'button';
+editButton.addEventListener('click', (e) => {
+    const li = e.target.parentNode;
+    const email = li.children[0].href.slice(7);
+    const name = li.children[0].textContent;
+    const comment = li.children[1].textContent.slice(8);
 
-  const emailInput = document.createElement("input");
-  emailInput.value = email;
-  const nameInput = document.createElement("input");
-  nameInput.value = name;
-  const commentInput = document.createElement("input");
-  commentInput.value = comment;
+    const emailInput = document.createElement('input');
+    emailInput.value = email;
+    emailInput.className = 'name';
+    const nameInput = document.createElement('input');
+    nameInput.value = name;
+    const commentInput = document.createElement('input');
+    commentInput.value = comment;
 
-  li.innerHTML = ""; //remove children
-  li.appendChild(nameInput);
-  li.appendChild(emailInput);
-  li.appendChild(commentInput);
-  li.appendChild(saveButton);
-  e.target.remove();
+    li.innerHTML = ''; //remove children
+    li.appendChild(nameInput);
+    li.appendChild(emailInput);
+    li.appendChild(commentInput);
+    li.appendChild(saveButton);
+    e.target.remove();
 });
 
-const saveButton = document.createElement("button");
-saveButton.textContent = "save";
-saveButton.type = "button";
-saveButton.addEventListener("click", (e) => {
-  const li = e.target.parentNode;
-  const name = li.children[0].value;
-  const email = li.children[1].value;
-  const comment = li.children[2].value;
+const saveButton = document.createElement('button');
+saveButton.textContent = 'save';
+saveButton.type = 'button';
+saveButton.addEventListener('click', (e) => {
+    const li = e.target.parentNode;
+    const name = li.children[0].value;
+    const email = li.children[1].value;
+    const comment = li.children[2].value;
 
-  const updatedMessage = createLi(name, email, comment);
-  li.replaceWith(updatedMessage);
+    const updatedMessage = createLi(name, email, comment);
+    li.replaceWith(updatedMessage);
 });
 
-const messageForm = document.getElementsByName("leave_message")[0];
-messageForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const name = event.target.name.value;
-  const email = event.target.email.value;
-  const message = event.target.message.value;
+const messageForm = document.getElementsByName('leave_message')[0];
+messageForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const message = event.target.message.value;
 
-  const messageSection = document.getElementById("messages");
-  const messageList = messageSection.getElementsByTagName("ul")[0];
+    const messageSection = document.getElementById('messages');
+    const messageList = messageSection.getElementsByTagName('ul')[0];
 
-  const newMessage = createLi(name, email, message);
+    const newMessage = createLi(name, email, message);
 
-  messageList.appendChild(newMessage);
+    messageList.appendChild(newMessage);
 
-  hideOrUnhide();
-  messageForm.reset();
+    hideOrUnhide();
+    messageForm.reset();
 });
 
 function createLi(name, email, message) {
-  const newMessage = document.createElement("li");
-  newMessage.innerHTML = `<a href="mailto:${email}">${name}</a><span> wrote: ${message}</span>`;
+    const newMessage = document.createElement('li');
+    newMessage.innerHTML = `<a href="mailto:${email}">${name}</a><span> wrote: ${message}</span>`;
 
-  const removeButton = document.createElement("button");
-  removeButton.innerText = "remove";
-  removeButton.type = "button";
-  removeButton.addEventListener("click", (event) => {
-    const entry = event.target.parentNode;
-    entry.remove();
-    hideOrUnhide();
-  });
+    const removeButton = document.createElement('button');
+    removeButton.innerText = 'remove';
+    removeButton.type = 'button';
+    removeButton.addEventListener('click', (event) => {
+        const entry = event.target.parentNode;
+        entry.remove();
+        hideOrUnhide();
+    });
 
-  newMessage.appendChild(editButton);
-  newMessage.appendChild(removeButton);
+    newMessage.appendChild(editButton);
+    newMessage.appendChild(removeButton);
 
-  return newMessage;
+    return newMessage;
 }
 
 function hideOrUnhide() {
-  const messageSection = document.getElementById("messages");
-  const messageList = messageSection.getElementsByTagName("ul")[0];
-  if (messageList.children.length === 0) {
-    messageSection.style.display = "none";
-  } else {
-    messageSection.style.display = "block";
-  }
+    const messageSection = document.getElementById('messages');
+    const messageList = messageSection.getElementsByTagName('ul')[0];
+    if (messageList.children.length === 0) {
+        messageSection.style.display = 'none';
+    } else {
+        messageSection.style.display = 'block';
+    }
 }
 
 hideOrUnhide();
